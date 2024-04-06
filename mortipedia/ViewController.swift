361312cec7c45305d944622cd68d68107b49f434
hoonjoo-park <hoonjoo94@naver.com>
2024-04-06 16:52:34 +0900

    Initial Commit

---
+++ mortipedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mortipedia
+//
+//  Created by Hoonjoo Park on 4/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
