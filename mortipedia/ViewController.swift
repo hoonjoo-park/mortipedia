89c0768bf23affdbc5904a8b8b72432015c3c151
hoonjoo-park <hoonjoo94@naver.com>
2024-04-07 03:59:58 +0900

    init project

--- mortipedia/ViewController.swift
+++ mortipedia/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  mortipedia
-//
-//  Created by Hoonjoo Park on 4/6/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
