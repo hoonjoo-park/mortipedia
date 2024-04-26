13ac26104d54e66f48f439e39f24d421c367e5e9
hoonjoo-park <hoonjoo94@naver.com>
2024-04-27 02:40:22 +0900

    feat: add searchContainer and fade I/O animation

--- mortipedia/Views/Characters/CharacterHeader.swift
+++ mortipedia/Views/Characters/CharacterHeader.swift
@@ -3,10 +3,15 @@ import FlexLayout
 
 class CharacterHeader: UIView {
     let rootFlexContainer = UIView()
+    let headerContainer = UIView()
     let title = MortiLabel(fontSize: 16, weight: .bold, color: Colors.text)
     let profileImageView = UIImageView()
     let searchButton = UIButton()
     let searchIcon = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+    let searchContainer = UIView()
+    let searchTextField = UITextField()
+    let cancelButton = UIButton()
+    let textFieldIcon = UIImageView(image: UIImage(systemName: "magnifyingglass"))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,20 +39,43 @@ class CharacterHeader: UIView {
         profileImageView.image = profileImage
         
         title.text = "Hello, \(username)!"
+        
+        searchContainer.alpha = 0
+        searchTextField.placeholder = "Enter character name here"
+        searchTextField.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        searchTextField.textColor = Colors.text
+        textFieldIcon.tintColor = Colors.text
+        
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.setTitleColor(Colors.primary, for: .normal)
+        cancelButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        cancelButton.addTarget(self, action: #selector(handlePressCancelButton), for: .touchUpInside)
     }
     
     
     private func configureFlexLayout() {
-        rootFlexContainer.flex.direction(.row).paddingVertical(20).paddingHorizontal(25).define { flex in
-            flex.addItem().direction(.row).alignItems(.center).gap(15).grow(1).define { flex in
-                flex.addItem().size(45).justifyContent(.center).alignItems(.center).cornerRadius(22.5).backgroundColor(Colors.accent).define { flex in
-                    flex.addItem(profileImageView).width(30).aspectRatio(of: profileImageView)
+        rootFlexContainer.flex.paddingVertical(20).paddingHorizontal(25).define { flex in
+            flex.addItem(headerContainer).direction(.row).alignItems(.center).define { flex in
+                flex.addItem().direction(.row).alignItems(.center).gap(15).grow(1).define { flex in
+                    flex.addItem().size(45).justifyContent(.center).alignItems(.center).cornerRadius(22.5).backgroundColor(Colors.accent).define { flex in
+                        flex.addItem(profileImageView).width(30).aspectRatio(of: profileImageView)
+                    }
+                    flex.addItem(title)
                 }
-                flex.addItem(title)
+                
+                flex.addItem(searchButton).size(45).justifyContent(.center).alignItems(.center).define { flex in
+                    flex.addItem(searchIcon).size(24).aspectRatio(of: searchIcon)
+                }
             }
             
-            flex.addItem(searchButton).size(45).justifyContent(.center).alignItems(.center).define { flex in
-                flex.addItem(searchIcon).size(24).aspectRatio(of: searchIcon)
+            flex.addItem(searchContainer).direction(.row).gap(15).position(.absolute).horizontally(0).paddingVertical(20).paddingHorizontal(25).define { flex in
+                
+                flex.addItem().direction(.row).alignItems(.center).grow(1).cornerRadius(12).padding(10).gap(12).backgroundColor(Colors.gray1).define { flex in
+                    flex.addItem(textFieldIcon)
+                    flex.addItem(searchTextField)
+                }
+                flex.addItem(cancelButton).padding(5)
             }
         }
     }
@@ -60,5 +88,25 @@ class CharacterHeader: UIView {
     }
     
     
-    @objc private func handlePressSearchButton() {}
+    @objc private func handlePressSearchButton() {
+        UIView.animate(withDuration: 0.15, delay: 0, options: [.curveEaseInOut], animations: {
+            self.headerContainer.alpha = 0
+        }, completion: { _ in
+            self.searchContainer.alpha = 0
+            UIView.animate(withDuration: 0.15, delay: 0, options: [.curveEaseInOut], animations: {
+                self.searchContainer.alpha = 1
+            })
+        })
+    }
+    
+    @objc private func handlePressCancelButton() {
+        UIView.animate(withDuration: 0.15, delay: 0, options: [.curveEaseInOut], animations: {
+            self.searchContainer.alpha = 0
+        }, completion: { _ in
+            self.headerContainer.alpha = 0
+            UIView.animate(withDuration: 0.15, delay: 0, options: [.curveEaseInOut], animations: {
+                self.headerContainer.alpha = 1
+            })
+        })
+    }
 }
