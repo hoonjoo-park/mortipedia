3a460fb917b88dfe1e4d709428c6851d2d252e73
hoonjoo-park <hoonjoo94@naver.com>
2024-05-26 22:12:00 +0900

    feat: implement configureViewController function in EpisodesVC

--- mortipedia/Views/Episodes/EpisodesVC.swift
+++ mortipedia/Views/Episodes/EpisodesVC.swift
@@ -5,6 +5,11 @@ class EpisodesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureViewController()
+    }
+    
+    
+    private func configureViewController() {
         view.backgroundColor = Colors.background
         navigationItem.title = "Episodes"
         navigationController?.navigationBar.prefersLargeTitles = true
