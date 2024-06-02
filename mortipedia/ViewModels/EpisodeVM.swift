81937646d865082529d9f76d54f89bd20aaad573
hoonjoo-park <hoonjoo94@naver.com>
2024-06-02 19:04:34 +0900

    feat: add updateSelectedEpisode function to Episode View Model

--- mortipedia/ViewModels/EpisodeVM.swift
+++ mortipedia/ViewModels/EpisodeVM.swift
@@ -17,11 +17,26 @@ class EpisodeVM {
         return episodesSubject.asObservable()
     }
     
+    private let selectedEpisodeSubject = BehaviorSubject<Episode?>(value: nil)
+    var selectedEpisode: Observable<Episode?> {
+        return selectedEpisodeSubject.asObservable()
+    }
+    
+    
     func getEpisodes() {
         NetworkManager.shared.fetchEpisodes().subscribe(onNext: { [weak self] response in
             guard let self = self else { return }
             
             self.episodesSubject.onNext(response)
         }).disposed(by: disposeBag)
+    }
+    
+    
+    func updateSelectedEpisode(id: Int) {
+        guard let currentEpisodes = try? episodesSubject.value() else { return }
+        
+        if let targetIndex = currentEpisodes.firstIndex(where: { $0?.id == id }) {
+            selectedEpisodeSubject.onNext(currentEpisodes[targetIndex])
+        }
     }
 }
