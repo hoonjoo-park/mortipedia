aa5c6cc90b11d8e9da4723dd36aa4693a80b0bd2
hoonjoo-park <hoonjoo94@naver.com>
2024-06-03 23:59:24 +0900

    feat: setup flexLayout for EpisodesVC

--- mortipedia/Views/Episodes/EpisodesVC.swift
+++ mortipedia/Views/Episodes/EpisodesVC.swift
@@ -2,11 +2,22 @@ import UIKit
 
 class EpisodesVC: UIViewController {
     private let episodeVM = EpisodeVM.shared
+    
+    private let rootFlexContainer = UIView()
+    private var episodesTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViewController()
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        rootFlexContainer.pin.all(view.safeAreaInsets)
+        rootFlexContainer.flex.layout()
     }
     
     
@@ -16,5 +27,15 @@ class EpisodesVC: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         episodeVM.getEpisodes()
+    }
+    
+    
+    private func configureUI() {
+        view.addSubview(rootFlexContainer)
+        view.backgroundColor = Colors.background
+        
+        rootFlexContainer.flex.define { flex in
+            flex.addItem()
+        }
     }
 }
