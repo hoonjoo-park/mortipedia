1399dc324791044fbd5e5d164c893683c49187aa
hoonjoo-park <hoonjoo94@naver.com>
2024-06-30 21:05:11 +0900

    refactor: change LoadingVC to LoadingView

---
+++ mortipedia/Views/Public/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  mortipedia
+//
+//  Created by Hoonjoo Park on 5/18/24.
+//
+
+import UIKit
+import FlexLayout
+
+class LoadingView: UIView {
+    let indicator = UIActivityIndicatorView(style: .large)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        indicator.startAnimating()
+        flex.backgroundColor(Colors.background).alignItems(.center).justifyContent(.center).define { flex in
+            flex.addItem(indicator)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        pin.all()
+        flex.layout()
+    }
+}
